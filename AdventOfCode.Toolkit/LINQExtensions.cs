6eddd1b2c294675101c986ce20bbf0dbd9eed1f6
lemm <ten.michal.k@gmail.com>
2015-12-17 07:29:14 +0100

    Day 17 solution added

--- AdventOfCode.Toolkit/LINQExtensions.cs
+++ AdventOfCode.Toolkit/LINQExtensions.cs
@@ -37,5 +37,37 @@ namespace AdventOfCode.Toolkit
                 return identitySelector(obj).GetHashCode();
             }
         }
+
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> source)
+        {
+            int length = source.Count();
+            if (length != 0)
+            {
+                int index = 0;
+                foreach (var item in source)
+                {
+                    var allOtherItems = source.RemoveAt(index);
+                    foreach (var permutation in allOtherItems.Permutations())
+                    {
+                        yield return new[] { item }.Concat(permutation);
+                    }
+                    index++;
+                }
+            }
+            else { yield return new T[0]; }
+        }
+
+        public static IEnumerable<T> RemoveAt<T>(this IEnumerable<T> source, int indexToRemove)
+        {
+            int index = 0;
+            foreach (T item in source)
+            {
+                if (index != indexToRemove)
+                {
+                    yield return item;
+                }
+                index++;
+            }
+        }
     }
 }
---
+++ AdventOfCode17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Toolkit;
+using System.IO;
+
+namespace AdventOfCode17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var sw = new StreamReader("../../input.txt"))
+            {
+                var list = sw.ForEachLine((line) => int.Parse(line) ).ToList();
+
+                var result = Enumerable
+                  .Range(1, (1 << list.Count) - 1)
+                  .Select(index => list.Where((item, idx) => ((1 << idx) & index) != 0).ToList());
+
+                //PART 1
+                var combinationsSatysfying = result.ToList().Where(comb => comb.Sum() == 150);
+
+                //PART 2
+                var minCount = combinationsSatysfying.Min(comb => comb.Count());
+                var minCombinations = combinationsSatysfying.Where(comb => comb.Count() == minCount);
+
+                System.Console.WriteLine($"Number of combinations(Part 1): {combinationsSatysfying.Count()}");
+                System.Console.WriteLine($"Different ways of minimal (Part 2): {minCombinations.Count()}");
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
