05cbb313bf691ee798f868096c05509a6a8dc12f
lemm <ten.michal.k@gmail.com>
2015-12-25 06:32:40 +0100

    Day 25 solution, readme and final screenshots added

---
+++ AdventOfCode25/Program.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            long index = 20151125;
+            while (true)
+            {
+                // System.Console.WriteLine($"[{x},{y}] = {index}");
+                index = NextValue(index);
+                x++;
+                y--;
+                if (y < 0)
+                {
+                    y = x;
+                    x = 0;
+                }
+                if (y == 3010 - 1 && x == 3019 - 1)
+                {
+                    System.Console.WriteLine(index);
+                    break;
+                }
+            }
+            System.Console.ReadLine();
+            
+        }
+
+        private static long NextValue(long index)
+        {
+            index *= 252533;
+            index %= 33554393;
+            return index;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Solutions for  
+Advent Of Code 2015  
+  
+Merry Christmas!
+
+![alt text][tree]
+![alt text][leaderboard]
+
+[tree]: https://github.com/lemmit/AdventOfCode15/raw/master/ChristmasTree.png
+[leaderboard]: https://github.com/lemmit/AdventOfCode15/raw/master/LeaderBoardAfterDay25.png
